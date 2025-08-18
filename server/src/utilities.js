2f1f8e4e75eb24efb090163c1b47d17de5afd849
Pham Tat Dat <convitduc6@gmail.com>
2025-08-19 02:01:33 +0700

    refactor: changed location of utils to src

--- server/src/utilities.js
+++ server/src/utilities.js
@@ -4,3 +4,8 @@ export function checkPasswordStrength(password) {
     // At least 8 chars, 1 uppercase, 1 lowercase, 1 number, 1 special char
     return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}$/.test(password);
 }
+
+//Used in Media Type and Media
+export function normalizeTypeName(name) {
+    return name.trim().toLowerCase().replace(/s$/, ""); // crude singularization
+}
