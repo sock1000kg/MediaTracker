089fdb8da46b7c9e21dcb4d6ece950c8c6dbc8c3
Pham Tat Dat <convitduc6@gmail.com>
2025-08-19 23:29:01 +0700

    feat: add rate limiter

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import rateLimit,  { ipKeyGenerator } from 'express-rate-limit'
 import cors from 'cors' //backend - frontend coms
 import 'dotenv/config'
 
@@ -11,9 +12,22 @@ import mediaRoutes from './routes/mediaRoutes.js'
 import mediaTypeRoutes from './routes/mediaTypeRoutes.js'
 import authMiddleWare from './middleWare/authMiddleware.js'
 
+const limiter = rateLimit({
+    windowMs: 60*1000, //1 minute
+    max: 30,
+    keyGenerator: (req) => {
+        if (req.userId) return req.userId
+        return ipKeyGenerator(req)
+    }, //Limit each user, fallback to IP if not logged in
+    message: { error: 'Too many requests'}
+})
+
 const app = express()
 
+//Middleware
+app.use(cors())
 app.use(express.json())
+app.use(limiter)
 
 //Serves Vite frontend when i have it eventually
 //app.use(express.static(path.join(__dirname, '../../client/dist')))
@@ -23,7 +37,6 @@ const __dirname = dirname(__filename) //get the dir from file url (src)
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, "..", "..", "client", "index.html"))
-    console.log("Server started on port: " + process.env.PORT)
 })
 
 //Routes
