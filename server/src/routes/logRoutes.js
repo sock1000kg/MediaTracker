d07a3bd00c9df38be4e7ff0b9878b5bb5b28a446
Pham Tat Dat <convitduc6@gmail.com>
2025-08-20 18:49:49 +0700

    feat: add updating and deleting logs

--- server/src/routes/logRoutes.js
+++ server/src/routes/logRoutes.js
@@ -4,11 +4,13 @@ import {
     getAllLogs, 
     createLog, 
     findLogOfUserByMediaId, 
-    findLogOfUserById,
+    findLogById,
     findMediaForUserById,
-    findMediaTypeForUserOrGlobal
-} from "./dbRoutes/dbRoutes.js";
-import { sanitizeNotes, sanitizeRating, sanitizeStatus } from '../utilities.js';
+    findMediaTypeForUserOrGlobal,
+    updateLog,
+    deleteLog
+} from "./dbRoutes/dbRoutes.js"
+import { sanitizeNotes, sanitizeRating, sanitizeStatus } from '../utilities.js'
 
 const router = express.Router()
 
@@ -20,8 +22,8 @@ router.get('/', async (req,res) => {
         console.log(logs)
         res.status(200).json(logs)
     } catch(error){
-        console.error(error);
-        res.status(500).json({ error: 'Failed to fetch user log' });
+        console.error(error)
+        res.status(500).json({ error: 'Failed to fetch user log' })
     }
 })
 
@@ -56,23 +58,53 @@ router.post('/', async (req,res) => {
         const log = await createLog(userId, mediaId, status, rating, notes)
         res.status(201).json(log)
     } catch(error){
-        console.error(error);
-        res.status(500).json({ error: 'Failed to create user log' });
+        console.error(error)
+        res.status(500).json({ error: 'Failed to create user log' })
     }
 })
 
+//Updates a log
 router.put('/:id', async (req,res) => {
-    const { status, rating, notes } = req.body
-    const id = parseInt(req.params.id)
+    const { status: rawStatus, rating: rawRating, notes: rawNotes } = req.body
+    const userId = req.userId
+    const logId = parseInt(req.params.id)
+
+    //Sanitization
+    const newStatus = sanitizeStatus(rawStatus)
+
+    const newRating = sanitizeRating(rawRating)
+
+    const newNotes = sanitizeNotes(rawNotes)
+    try{
+        //Check if log exists or belong to that user id
+        const existingLog = await findLogById(logId)
+        if(!existingLog) return res.status(404).json({ error: 'Log does not exist'})
+        if(existingLog.userId !== userId) return res.status(401).json({ error: 'You do not own this log'})
+
+        const updated = await updateLog(logId, newStatus, newRating, newNotes)
+        return res.status(200).json(updated)
+    }catch(error){
+        console.error(error)
+        res.status(500).json({ error: 'Failed to update user log' })
+    }
+})
+
+//Deletes log
+router.delete('/:id', async (req,res) => {
+    const logId = parseInt(req.params.id)
+    const userId = req.userId
 
     try{
-        //Check if log exists for that id
-        const existingLog = findLogOfUserById(id)
+        //Check if log exists or belong to user
+        const existingLog = await findLogById(logId)
         if(!existingLog) return res.status(404).json({ error: 'Log does not exist'})
+        if(existingLog.userId !== userId) return res.status(401).json({ error: 'You do not own this log'})
 
-        
-    }catch(error){
-
+        await deleteLog(logId)
+        res.status(200).json({ message: 'Log deleted' })
+    }catch(error) {
+        console.error(error)
+        res.status(500).json({ error: 'Failed to delete user log' })
     }
 })
 
