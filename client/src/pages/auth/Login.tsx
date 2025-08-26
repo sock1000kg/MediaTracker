52717e6d31fd524473de9f47f38f9e94edd8ceb5
Pham Tat Dat <convitduc6@gmail.com>
2025-08-27 01:26:41 +0700

    feat: connect auth routes to frontend

---
+++ client/src/pages/auth/Login.tsx
@@ -0,0 +1,108 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
+export default function Login() {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState<string | null>(null)
+
+    const navigate = useNavigate()
+
+    const handleSubmit = async (e: React.FormEvent) => {
+      e.preventDefault()
+      setError(null)
+
+      try {
+          const res = await fetch("http://localhost:5000/auth/login", { 
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ username, password }),
+            credentials: "include",
+          })
+
+          const data = await res.json()
+
+          if (res.ok && data.token) {
+            localStorage.setItem("token", data.token)
+
+            console.log("Login success:", data)
+
+            navigate("/dashboard")
+          } else {
+            setError(data.error || "Login failed")
+          }
+      } catch (error) {
+          console.error("Error:", error)
+          setError("Something went wrong")
+      }
+  }
+
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-stone-200">
+      <Card className="w-full max-w-sm shadow-2xl">
+
+        <CardHeader>
+          <CardTitle className="text-2xl font-bold text-center">Login</CardTitle>
+        </CardHeader>
+
+        <CardContent>
+          <form onSubmit={handleSubmit} className="space-y-4">
+
+            <div className="space-y-2">
+                <Label htmlFor="username">Username</Label>
+                <Input
+                    id="username"
+                    type="username"
+                    placeholder="YourUsername"
+                    value={username}
+                    onChange={(e) => {
+                        const value = e.target.value
+                        setUsername(value)
+
+                        // Check if there are spaces
+                        if(/\s/.test(value)) {
+                            setError("Username cannot contain spaces")
+                        } else {
+                            setError(null)
+                        }
+                    }}
+                    required
+                />
+                </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                id="password"
+                type="password"
+                placeholder="••••••••"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+
+            <Button type="submit" className="w-full bg-stone-800 hover:bg-stone-700">Sign in</Button>
+
+          </form>
+
+          {/* Error message */}
+          {error && (
+            <p className="mt-2 text-center text-sm text-red-500">
+              {error}
+            </p>
+          )}
+
+          {/* Switch to register */}
+          <p className="mt-4 text-center text-sm text-muted-foreground">
+            Don’t have an account? <a href="/register" className="underline">Sign up</a>
+          </p>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
---
+++ client/src/pages/auth/Register.tsx
@@ -0,0 +1,128 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
+export default function Register() {
+    const [username, setUsername] = useState("")
+    const [displayName, setDisplayName] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState<string | null>(null)
+
+    const navigate = useNavigate()
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault()
+        setError(null)
+
+        try {
+            const res = await fetch("http://localhost:5000/auth/register", { 
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ username, password, displayName }),
+            credentials: "include",
+            })
+
+            const data = await res.json()
+
+            if (res.ok && data.token) {
+            localStorage.setItem("token", data.token)
+
+            console.log("Login success:", data)
+
+            navigate("/dashboard")
+            } else {
+            setError(data.error || "Login failed")
+            }
+        } catch (error) {
+            console.error("Error:", error)
+            setError("Something went wrong")
+        }
+    }
+
+    return (
+        <div className="flex min-h-screen items-center justify-center bg-stone-200">
+        <Card className="w-full max-w-sm shadow-2xl">
+            
+            <CardHeader>
+            <CardTitle className="text-2xl font-bold text-center">Register</CardTitle>
+            </CardHeader>
+
+            <CardContent>
+            <form onSubmit={handleSubmit} className="space-y-4">
+                <div className="space-y-2">
+                <Label htmlFor="username">Username</Label>
+                <Input
+                    id="username"
+                    type="username"
+                    placeholder="YourUsername"
+                    value={username}
+                    onChange={(e) => {
+                        const value = e.target.value
+                        setUsername(value)
+
+                        // Check if there are spaces
+                        if(/\s/.test(value)) {
+                            setError("Username cannot contain spaces")
+                        } else {
+                            setError(null)
+                        }
+                    }}
+                    required
+                />
+                </div>
+
+                <div className="space-y-2">
+                <Label htmlFor="displayName">Display Name</Label>
+                <Input
+                    id="displayName"
+                    type="text"
+                    placeholder="Your Display Name"
+                    value={displayName}
+                    onChange={(e) => {
+                        const value = e.target.value
+                        setDisplayName(value)
+
+                        // check if the whole thing is just spaces
+                        if (value.trim() === "") {
+                            setError("Display name cannot be just spaces")
+                        } else {
+                            setError(null)
+                        }
+                    }}
+                    required
+                />
+                </div>
+
+                <div className="space-y-2">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                    id="password"
+                    type="password"
+                    placeholder="••••••••"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+                </div>
+                <Button type="submit" className="w-full bg-stone-800 hover:bg-stone-700">Sign up</Button>
+            </form>
+
+            {/* Error message */}
+            {error && (
+                <p className="mt-2 text-center text-sm text-red-500">
+                {error}
+                </p>
+            )}
+
+            {/* Switch to login */}
+            <p className="mt-4 text-center text-sm text-muted-foreground">
+                Already have an account? <a href="/login" className="underline">Sign in</a>
+            </p>
+            </CardContent>
+        </Card>
+        </div>
+    )
+}
