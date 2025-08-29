422b0b2d5b7a1744187e8c23e4b1aa131bf9af52
Pham Tat Dat <convitduc6@gmail.com>
2025-08-29 12:57:29 +0700

    feat: add new button varient

--- client/src/components/ui/button.tsx
+++ client/src/components/ui/button.tsx
@@ -20,6 +20,7 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
+        amber: "bg-amber-600 text-amber-50 shadow-xs hover:bg-amber-700"
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
