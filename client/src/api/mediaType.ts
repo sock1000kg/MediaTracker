e1143a9112cf39834964a65073d740761ab245ff
Pham Tat Dat <convitduc6@gmail.com>
2025-08-31 03:37:55 +0700

    feat: add media fetching

--- client/src/api/mediaType.ts
+++ client/src/api/mediaType.ts
@@ -1,3 +1,21 @@
+export async function fetchMediaTypes(token: string) {
+  const res = await fetch("http://localhost:5000/media-type", {
+    headers: {
+      "Authorization": `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  })
+
+  if (!res.ok) {
+    const errData = await res.json().catch(() => ({}))
+    throw {
+        status: res.status,
+        message: errData.message || "Failed to fetch media types",
+    }
+  }
+
+  return res.json()
+}
 
 
 export async function createMediaType(name: string, token: string | null) {
