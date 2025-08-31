c77783724749e6aec1d19bee5599298231d6f18e
Pham Tat Dat <convitduc6@gmail.com>
2025-08-31 21:26:51 +0700

    chore: change styling

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -74,14 +74,14 @@ export default function Homepage() {
         <div className="flex min-h-screen bg-stone-100"> 
 
           {/* Display user's contents */}
-          <div className="flex-1 space-y-8">
+          <div className="flex-1">
             {activeTab === "logs" && <LogsSection groupedLogs={groupedLogs} />}
             {activeTab === "medias" && <MediasSection />}
             {activeTab === "mediaTypes" && <MediaTypesSection />}
           </div>
 
           {/* Right side bar */}
-          <div className="flex flex-col w-40 bg-stone-300 space-y-4">
+          <div className="flex flex-col w-40 bg-stone-300">
             <Button 
               variant={activeTab === "logs" ? "boldedLink" : "link"}
               className="text-stone-600 w-full rounded-none shadow-[0_2px_2px_-2px_rgba(0,0,0,0.2)] hover:underline" 
