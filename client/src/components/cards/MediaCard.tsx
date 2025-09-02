02391b39208c8f6e0ceeff3fbbb87759f88d27f1
Pham Tat Dat <convitduc6@gmail.com>
2025-09-03 02:29:14 +0700

    feat: display metadata in MediaCards

--- client/src/components/cards/MediaCard.tsx
+++ client/src/components/cards/MediaCard.tsx
@@ -1,3 +1,5 @@
+import {Fragment} from 'react'
+
 import type { Media } from "@/types/media"
 
 import { Button } from "../ui/button"
@@ -11,6 +13,7 @@ interface MediaCardProps {
 export function MediaCard({ media, onEdit, onDelete}: MediaCardProps) {
   return (
     <li key={media.id} className="p-3 bg-white rounded-xl shadow-sm border border-stone-200">
+        {/* Header */}
         <div className="flex justify-between items-center">
             {/* Left side info */}
             <div className="flex items-center gap-1">
@@ -47,6 +50,33 @@ export function MediaCard({ media, onEdit, onDelete}: MediaCardProps) {
                 Delete
                 </Button>
             </div>
+        </div>
+
+        {/* Info */}
+        <div className="flex-col text-sm text-stone-600 gap-2">
+            <div className="flex gap-2 ">
+                {media.creator && <span>{media.creator}</span>}
+                {media.creator && media.year && <span>•</span>}
+                {media.year && <span>{media.year}</span>}
+            </div>
+            <div className=" flex gap-2">
+                {Object.entries(media.metadata || {}) //converts to array of [key, value] pairs
+                    .filter(([key, value]) => value != null && value !== '') // Filter out empty values
+                    .map(([key, value], index, arr) => (
+                        <Fragment key={key}>
+                            <span className='flex gap-1 capitalize'>
+                                <span className="font-semibold">
+                                    {key}:
+                                </span>
+                                {String(value)}
+                            </span>
+                            {index < arr.length - 1 && <span>•</span>}
+                        </Fragment>
+                    ))
+                }
+
+            </div>
+
         </div>
     </li>
   )
