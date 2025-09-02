87920fe42ca6ee196644bdc9a9b50340f929616f
Pham Tat Dat <convitduc6@gmail.com>
2025-09-02 18:55:49 +0700

    chore: add LogCardProps to standardize props passing

--- client/src/components/cards/LogCard.tsx
+++ client/src/components/cards/LogCard.tsx
@@ -1,17 +1,24 @@
 import type { Log } from "@/types/media"
 
-export function LogCard({ log }: { log: Log }) {
+interface LogCardProps {
+  log: Log
+}
+
+export function LogCard({log}: LogCardProps ) {
   return (
-    <div>
+    <li>
       <div className="rounded-lg border p-3 shadow-sm bg-white border-stone-200">
+
         <div className="flex justify-between items-center">
           <p className="font-bold text-stone-800">{log.media.title}</p>
           <span className="text-sm rounded-2xl px-2 py-0.5 bg-amber-100 text-amber-800 capitalize">
             {log.status}, Rating: {log.rating}
           </span>
         </div>
+
         <p className="text-md text-stone-800"> {log.notes} </p>
+        
       </div>
-    </div>
+    </li>
   )
 }
