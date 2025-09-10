6fa823919b790e5bb6a680e9713e9fd0e8d3984e
Pham Tat Dat <convitduc6@gmail.com>
2025-09-11 01:29:43 +0700

    feat: add search routes for book, add createMediaAndLog to create a new media for the searched up media

---
+++ server/src/controllers/searchControllers.ts
@@ -0,0 +1,133 @@
+import { createMediaAndLogSchema, googleBooksResponseSchema, SearchResult, searchResultsSchema } from '@/schemas/searchSchemas'
+import { validateSchema } from '@/utilities'
+import { Request, Response } from 'express'
+import { ZodError } from 'zod'
+import { createLog, createMedia, findLogOfUserByMediaId, findMediaBySource, updateLog } from './dbCalls/dbControllers'
+
+export const createMediaAndLog = async (req: Request, res: Response) => {
+    const userId = req.userId
+    if (!userId) return res.status(401).json({ message: "Unauthorized" })
+
+    try {
+        console.log("BODY RECEIVED:", req.body)
+        const { mediaData, logData } = validateSchema(createMediaAndLogSchema, req.body)
+
+        // Check if media exists in db, if not create a system media
+        let media = await findMediaBySource(mediaData.sourceId, mediaData.source)
+        if (!media) media = await createMedia(
+            mediaData.title,
+            mediaData.mediaType,
+            mediaData.creator,
+            mediaData.year,
+            mediaData.source,
+            mediaData.sourceId,
+            mediaData.sourceRating,
+            mediaData.ratingsCount,
+            mediaData.description,
+            mediaData.metadata,
+            mediaData.imageUrl,
+            0 //systemId
+        )
+
+        //Check if log exists
+        const existingLog = await findLogOfUserByMediaId(userId, media.id)
+        let newLog
+        if (existingLog) {
+            //Update log
+            newLog = await updateLog(existingLog.id, logData.status, logData.rating, logData.notes)
+        }
+        else {
+            // Create log
+            newLog = await createLog(userId, media.id, logData.status, logData.rating, logData.notes)
+        }
+        
+        res.status(201).json({ media, newLog })
+
+    } catch (error) {
+        console.dir(error, { depth: null })
+
+        if (error instanceof ZodError) {
+        // return first validation error as JSON
+            return res.status(400).json({
+                message: error.issues[0]?.message || "Validation failed",
+                errors: error.issues
+            })
+        }
+
+        console.error(error)
+        res.status(500).json({ message: "Failed to create media and log" })
+    }
+}
+
+
+export async function searchBooks(req: Request, res: Response) {
+    const userId = req.userId
+    if (!userId) return res.status(401).json({ message: "Unauthorized: missing userId" })
+
+    const q = req.query.q as string
+    if(!q) return res.status(404).json({ message: "Missing query" })
+
+    // startIndex is google's search index (the index of the top searches array)
+    const startIndex = parseInt(req.query.startIndex as string) || 0
+    const maxResults = 15
+
+    try{
+        //Form the API call
+        const url = new URL("https://www.googleapis.com/books/v1/volumes")
+        url.searchParams.set("q", q)
+        url.searchParams.set("maxResults", "15")
+        url.searchParams.set("startIndex", startIndex.toString())
+
+        if(process.env.GOOGLE_BOOKS_API_KEY) {
+            console.log("Books API key exists")
+            url.searchParams.set("key", process.env.GOOGLE_BOOKS_API_KEY)
+        }
+
+        const response = await fetch(url.toString())
+        if (!response.ok) {
+            return res.status(response.status).json({ error: "Google Books API error" })
+        }
+
+        //Parse raw data
+        const raw = await response.json()
+        const parsed = validateSchema(googleBooksResponseSchema, raw)
+
+        //Map the raw data to array of individual items
+        const rawResults =
+            parsed.items?.map((item): SearchResult => {
+                const { title, authors, publishedDate, averageRating, ratingsCount, description, imageLinks } = item.volumeInfo
+
+                return {
+                    title,
+                    creator: authors ? authors.join() : null,
+                    year: publishedDate ? publishedDate.split("-")[0] : null,
+                    description: description ?? null,
+                    imageUrl: imageLinks ? imageLinks.thumbnail : null,
+                    source: "google_books",
+                    sourceId: item.id,
+                    sourceRating: averageRating !== null ? Math.round((averageRating / 5) * 100) : null,
+                    ratingsCount: ratingsCount ?? null,
+                    metadata: {
+                        pageCount: item.volumeInfo.pageCount,
+                        categories: item.volumeInfo.categories,
+                        publisher: item.volumeInfo.publisher,
+                    },
+            }
+        }) ?? []
+
+        //Validate 
+        const results = validateSchema(searchResultsSchema, rawResults)
+        if (results) console.log(results)
+        res.status(200).json(results)
+    }catch(error){
+        if (error instanceof ZodError) {
+        // return first validation error as JSON
+            return res.status(400).json({
+                message: error.issues[0]?.message || "Validation failed",
+                errors: error.issues
+            })
+        }
+        console.error(error)
+        res.status(500).json({ message: "Failed to search for books" })
+    }
+}
---
+++ server/src/routes/searchRoutes.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+
+import { createMediaAndLog, searchBooks } from '@/controllers/searchControllers'
+
+const router = express.Router()
+
+router.put('/media-log', createMediaAndLog)
+
+router.get('/books', searchBooks)
+
+export default router
