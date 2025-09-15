bb6aa8b3ee1a007b41ba7276e9da9cf6618e046b
Pham Tat Dat <convitduc6@gmail.com>
2025-09-15 11:13:12 +0700

    refactor: make limiter a seperate middleware file

--- server/src/middleWare/rateLimiter.ts
+++ server/src/middleWare/rateLimiter.ts
@@ -1,11 +1,15 @@
 import rateLimit, { ipKeyGenerator, RateLimitRequestHandler } from "express-rate-limit"
 
-export const limiter: RateLimitRequestHandler = rateLimit({
-    windowMs: 60 * 1000, // 1 minute
-    max: 30,
-    keyGenerator: (req) => {
-        if (req.ip !== undefined) return ipKeyGenerator(req.ip)
-        return ipKeyGenerator('unknown')
-    },
-    message: { error: 'Too many requests' }
-})
+function limiter(): RateLimitRequestHandler { 
+    return rateLimit({
+        windowMs: 60 * 1000, // 1 minute
+        max: 30,
+        keyGenerator: (req) => {
+            if (req.ip !== undefined) return ipKeyGenerator(req.ip)
+            return ipKeyGenerator('unknown')
+        },
+        message: { error: 'Too many requests' }
+    })
+}
+
+export default limiter
