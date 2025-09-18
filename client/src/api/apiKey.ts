6e716ae624548a6be3a6c8f0e3684733a542e4ee
Pham Tat Dat <convitduc6@gmail.com>
2025-09-18 11:49:59 +0700

    feat: add apiKey dbCalls

---
+++ client/src/api/apiKey.ts
@@ -0,0 +1,35 @@
+import type { ApiKey } from "@/types/main";
+import { apiFetch } from "./clientWrapper"
+
+export function getApiKeys(): Promise<ApiKey[]> {
+    return apiFetch(`/api-key`)
+}
+
+export function addApiKey(apiKey: ApiKey): Promise<ApiKey> {
+    return apiFetch(`/api-key`, {
+        method: "POST",
+        body: JSON.stringify({
+            key: apiKey.key,
+            service: apiKey.service
+        })
+    })
+}
+
+export function updateApiKey(apiKey: ApiKey): Promise<ApiKey> {
+    return apiFetch(`/api-key`, {
+        method: "PUT",
+        body: JSON.stringify({
+            key: apiKey.key,
+            service: apiKey.service
+        })
+    })
+}
+
+export function deleteApiKey(apiKey: ApiKey): Promise<{ message: string }> {
+    return apiFetch(`/api-key`, {
+        method: "DELETE",
+        body: JSON.stringify({
+            service: apiKey.service
+        })
+    })
+}
